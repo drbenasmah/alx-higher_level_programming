e440f1e206e7ef505701eacc678f1abdd7363fb6
drbenasmah <drbenasmah2016@gmail.com>
2023-03-28 02:28:45 +0200

    updated solution

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -2,21 +2,6 @@
 
 
 def magic_calculation(a, b):
-	 """Perform a magic calculation based on two input values.
-
-    This function calculates a magic value by raising the first input value
-    (a) to the power of the second input value (b), and then dividing by the
-    range of integers from 1 to 3. If the value of i in the loop is greater
-    than a, a ValueError is raised and the magic value is calculated by
-    adding the values of a and b together.
-
-    Args:
-        a: A numeric input value.
-        b: A numeric input value.
-
-    Returns:
-        The magic value calculated by the function.
-    """
     result = 0
     for i in range(1, 3):
         try:
