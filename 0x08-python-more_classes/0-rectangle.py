0e71eecaa6a3ca82da260a27135565a18b694f3f
drbenasmah <drbenasmah2016@gmail.com>
2023-04-03 22:15:28 +0200

    rectangle update

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,32 @@
 #!/usr/bin/python3
 
-# This module defines a Rectangle class
-
 class Rectangle:
-    """Represents a rectangle."""
-    pass
+    """
+    Rectangle: A class used to represent a rectangle.
+
+    Attributes:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+    """
+    def __init__(self, width=0, height=0):
+        """
+        Initializes a new Rectangle object with the given width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """Calculates the area of the rectangle."""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Calculates the perimeter of the rectangle."""
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """Returns a string representation of the rectangle."""
+        return "Rectangle({}, {})".format(self.width, self.height)
