dd2f590f36141b635abea2ab42937e438436d7ef
drbenasmah <drbenasmah2016@gmail.com>
2023-04-04 23:06:51 +0200

    task 0 update

--- 0x08-python-more_classes/0-main.py
+++ 0x08-python-more_classes/0-main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+# Import the Rectangle class from 0-rectangle.py
 Rectangle = __import__('0-rectangle').Rectangle
 
+# Create a new Rectangle object and print its type and attributes
 rect = Rectangle()
 print(type(rect))
 print(rect.__dict__)
