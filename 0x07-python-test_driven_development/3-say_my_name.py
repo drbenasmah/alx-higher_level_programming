b2224c8a14786ce62db0e22c0bb535fe39a34ecb
drbenasmah <drbenasmah2016@gmail.com>
2023-04-06 01:11:08 +0200

    task 3 update

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,25 +1,11 @@
 #!/usr/bin/python3
+from sys import argv
+say_my_name = __import__('3-say_my_name').say_my_name
 
-def say_my_name(first_name, last_name=""):
-    """
-    Print a name.
-
-    Args:
-        first_name (str): The first name to print.
-        last_name (str): The last name to print.
-
-    Raises:
-        TypeError: If either of first_name or last_name are not strings.
-    """
-
-    # Check if first_name and last_name are strings
-    if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
-
-    # Print the name
-    if last_name:
-        print("My name is {} {}".format(first_name, last_name))
+if __name__ == "__main__":
+    if len(argv) == 3:
+        say_my_name(argv[1], argv[2])
+    elif len(argv) == 2:
+        say_my_name(argv[1])
     else:
-        print("My name is {}".format(first_name))
+        print("Usage: ./3-main.py <first name> <last name>")
