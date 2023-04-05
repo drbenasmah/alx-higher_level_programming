39d168981ccf93a14b78b5604d041c3b6d3171ac
drbenasmah <drbenasmah2016@gmail.com>
2023-04-06 01:18:31 +0200

    task 3 update

--- 0x07-python-test_driven_development/tests/3-main.py
+++ 0x07-python-test_driven_development/tests/3-main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def say_my_name(first_name, last_name=""):
-    """
-    Prints My name is <first name> <last name>
-    """
-    if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+say_my_name = __import__('3-say_my_name').say_my_name
+
+say_my_name("John", "Smith")
+say_my_name("Walter", "White")
+say_my_name("Bob")
+try:
+    say_my_name(12, "White")
+except Exception as e:
+    print(e)
