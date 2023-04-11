dc48cc6436ef6f01498638aacb83253c8e95c5ed
drbenasmah <drbenasmah2016@gmail.com>
2023-04-11 23:37:51 +0200

    task 0 and Readme

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 """
-This script defines a function called "lookup" which takes an object as input
-and returns a list of available attributes and methods of that object.
+This script defines
 """
+
 
 def lookup(obj):
     """
-    This function returns a list of available attributes and methods of an object.
-    
-    Args:
-        obj: An object for which the attributes and methods need to be looked up.
-    
-    Returns:
-        A list of strings containing the names of available attributes and methods of the object.
+    This function returns a list of available attributes .
     """
+
     return dir(obj)
