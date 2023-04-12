aaba9c8ed54bd59e9eab802d863378fc3b81198e
drbenasmah <drbenasmah2016@gmail.com>
2023-04-12 22:47:11 +0200

    task 0 Betty check

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""Defines a text file-reading function."""
 
 
 def read_file(filename=""):
-    """Print the contents of a UTF8 text file to stdout."""
+    """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
