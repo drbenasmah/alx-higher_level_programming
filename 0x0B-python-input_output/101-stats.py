d7c29bc8d20dbe171e3589a8cc0335ab84f653ed
drbenasmah <drbenasmah2016@gmail.com>
2023-04-13 02:17:17 +0200

    task 10 - 101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 """This reads from standard input and computes metrics.
-After every ten lines prints the following statistics:
-    - Total file size up to that point.
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - The total file size up to that point.
     - Count of read status codes up to that point.
 """
 
